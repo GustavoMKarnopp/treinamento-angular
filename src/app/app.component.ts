074749ac44867f03411c9e7bdb13383eba7ed14b
80882485 <gustavo.karnopp@br.ey.com>
2023-01-20 11:41:23 -0300

    Exemplo de apresentacao de ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
+    {{valor}}
+    <button (click)="adicionar()" >Adicionar</button>
     <router-outlet></router-outlet>
-    <app-title title="Olá GUS"></app-title>
   `
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck, AfterViewChecked, AfterContentInit, AfterViewInit, AfterContentChecked{
+  public valor: number = 1;
+  
   constructor() {}
+
+  public adicionar(): number{
+     return this.valor += 1
+  }
 
   //VOID CARACTERIZA QUE NÃO TERA NENHUM RETORNO
   ngOnInit(): void{
-    console.log(1)
+  }
+  ngDoCheck(): void{
+    console.log('ngDoCheck', 'ESTARÁ SEMPRE VERIFICANDO SE HOUVE ALGUMA ALTERACAO')
+  }
+  ngAfterContentInit(): void{
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void{
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(): void{
+    console.log('ngAfterViewInit', 'ASSIM QUE CARREGAR TODA A PÁGINA ELE CHAMA A FUNCAO')
+  }
+  ngAfterViewChecked(): void{
+    console.log('ngAfterViewChecked')
   }
 }
