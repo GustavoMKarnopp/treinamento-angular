8ff0b90e3545a4ff67d25e3ad8ddb71375d2f327
80882485 <gustavo.karnopp@br.ey.com>
2023-01-20 14:38:54 -0300

    Finalizando ciclos de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,25 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    {{valor}}
-    <button (click)="adicionar()" >Adicionar</button>
     <router-outlet></router-outlet>
+    <!--UTILIZAMOS DESTA MANEIRA PARA SE FAZER VALIDACOES DENTRO DOS COMPONENTES-->
+    <app-title *ngIf="destruir"></app-title>
+    <br>
+    <button (click)="destruirComponente()">Destruir componente</button>
   `
 })
-export class AppComponent implements OnInit, DoCheck, AfterViewChecked, AfterContentInit, AfterViewInit, AfterContentChecked{
-  public valor: number = 1;
-  
+export class AppComponent implements OnInit {
   constructor() {}
 
-  public adicionar(): number{
-     return this.valor += 1
-  }
+  public destruir: boolean = true;
 
   //VOID CARACTERIZA QUE NÃO TERA NENHUM RETORNO
   ngOnInit(): void{
   }
-  ngDoCheck(): void{
-    console.log('ngDoCheck', 'ESTARÁ SEMPRE VERIFICANDO SE HOUVE ALGUMA ALTERACAO')
-  }
-  ngAfterContentInit(): void{
-    console.log('ngAfterContentInit')
-  }
-  ngAfterContentChecked(): void{
-    console.log('ngAfterContentChecked')
-  }
-  ngAfterViewInit(): void{
-    console.log('ngAfterViewInit', 'ASSIM QUE CARREGAR TODA A PÁGINA ELE CHAMA A FUNCAO')
-  }
-  ngAfterViewChecked(): void{
-    console.log('ngAfterViewChecked')
+
+  public destruirComponente() {
+    this.destruir = false
   }
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit, OnChanges{ //OnInit, trata-se do ciclo de vida do algular
+export class TitleComponent implements OnInit, OnChanges, OnDestroy{ //OnInit, trata-se do ciclo de vida do algular
   @Input() public title: string = 'Thomas Karnopp'
 constructor(){}
 
@@ -15,5 +15,10 @@ constructor(){}
   // ngOnChanges UTILIZAMOS PARA QUANDO RECEBEMOS VALOR DE FORA POR EXEMPLO EM UM INPUT
   ngOnChanges(): void {
     console.log('RECEBEU UM VALOR DE FORA!')
+  }
+  ngOnDestroy(): void {
+    console.log('FOI DESTRUIDO!', 'AVISA QUANDO O COMPONENTE FOI DESTRUIDO')
+    //PODEMOS UTILIZA-LO PARA QUE QUANDO FORMOS FAZER UMA ACAO DE DESTRUIR,
+    // PODEMOS PASSAR UMA MENSAGEM QUESTIONANDO SE É EXATAMENTE ISTO, TIPO UM POPUP
   }
 }
