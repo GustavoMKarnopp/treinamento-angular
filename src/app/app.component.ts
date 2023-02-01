0bdbb613e0306e9db6ac86119e36057831687b72
80882485 <gustavo.karnopp@br.ey.com>
2023-02-01 11:24:10 -0300

    Estruturando projetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-data-biding></app-data-biding>
     <router-outlet></router-outlet>
-    <!--UTILIZAMOS DESTA MANEIRA PARA SE FAZER VALIDACOES DENTRO DOS COMPONENTES-->
-    <app-title *ngIf="destruir"></app-title>
-    <br>
-    <button (click)="destruirComponente()">Destruir componente</button>
   `
 })
 export class AppComponent implements OnInit {
   constructor() {}
 
-  public destruir: boolean = true;
-
   //VOID CARACTERIZA QUE NÃO TERA NENHUM RETORNO
   ngOnInit(): void{
-  }
-
-  public destruirComponente() {
-    this.destruir = false
   }
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,24 +1,16 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit, OnChanges, OnDestroy{ //OnInit, trata-se do ciclo de vida do algular
+export class TitleComponent implements OnInit{ //OnInit, trata-se do ciclo de vida do algular
   @Input() public title: string = 'Thomas Karnopp'
 constructor(){}
 
   ngOnInit(): void {
     
   }
-  // ngOnChanges UTILIZAMOS PARA QUANDO RECEBEMOS VALOR DE FORA POR EXEMPLO EM UM INPUT
-  ngOnChanges(): void {
-    console.log('RECEBEU UM VALOR DE FORA!')
-  }
-  ngOnDestroy(): void {
-    console.log('FOI DESTRUIDO!', 'AVISA QUANDO O COMPONENTE FOI DESTRUIDO')
-    //PODEMOS UTILIZA-LO PARA QUE QUANDO FORMOS FAZER UMA ACAO DE DESTRUIR,
-    // PODEMOS PASSAR UMA MENSAGEM QUESTIONANDO SE É EXATAMENTE ISTO, TIPO UM POPUP
-  }
+ 
 }
