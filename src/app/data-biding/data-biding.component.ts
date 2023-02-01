f895e5a99c549f53381388d117dfea3af3fd8114
80882485 <gustavo.karnopp@br.ey.com>
2023-02-01 11:51:16 -0300

    Executando teste e validando intepolátion

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-biding',
   templateUrl: './data-biding.component.html',
   styleUrls: ['./data-biding.component.scss']
 })
-export class DataBidingComponent {
+export class DataBidingComponent implements OnInit {
+
+  public name: string = "Gustavo"
+  public idade: Number = 25
+  public resposta: string = ''
+
+
+  constructor() {}
+
+  ngOnInit(): void {// RETORNA TODA AS VEZES QUE CARREGAR A PÁGINA
+    if (this.idade > 29 ) {
+      this.resposta = 'Sim'
+    }else{
+      this.resposta = 'Não'
+    };
+  }
 
 }
